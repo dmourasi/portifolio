d9ea84a1cdf3607281ca932672a1d4fe55199838
dmourasi <dmouraslv@gmail.com>
2024-09-09 18:04:29 -0300

    imagens

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -1,13 +1,33 @@
-
+import { styled } from "@mui/material"
+import Avatar from "../../../../assets/imagens/avatar.jpg"
 
 const Hero = () => {
 
-    return (
-      <>
-        Hero
-      </>
-    )
-  }
-  
-  export default Hero
-  
+  const StyledHero = styled("div")(() => ({
+    backgroundColor: "black"
+  }))
+
+  const StyledImg = styled("img")(() => ({
+    with: "30%",
+    borderRadius: "50%"
+  }))
+
+  return (
+    <>
+      <StyledHero>
+
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <StyledImg src={Avatar} />
+          </Grid>
+          <Grid item xs={4}>
+            xs=4
+          </Grid>
+        </Grid>
+
+      </StyledHero>
+    </>
+  )
+}
+
+export default Hero
