28e51ea6737309636049cfc0cd102ed49f66a4b4
dmourasi <dmouraslv@gmail.com>
2024-09-09 18:08:21 -0300

    imagens

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -1,33 +1,34 @@
-import { styled } from "@mui/material"
+import { Grid, styled } from "@mui/material"
 import Avatar from "../../../../assets/imagens/avatar.jpg"
 
 const Hero = () => {
 
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black"
-  }))
+    const StyledHero = styled("div")(()=> ({
+        backgroundColor: "black"
+    }))
+    
+    const StyledImg = styled("img")(()=> ({
+        with: "30%",
+        borderRadius: "50%"
+    }))
 
-  const StyledImg = styled("img")(() => ({
-    with: "30%",
-    borderRadius: "50%"
-  }))
-
-  return (
-    <>
-      <StyledHero>
-
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <StyledImg src={Avatar} />
+    return (
+      <>
+        <StyledHero>
+          
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <StyledImg src={Avatar} />
+            </Grid>
+            <Grid item xs={4}>
+              xs=4
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            xs=4
-          </Grid>
-        </Grid>
-
-      </StyledHero>
-    </>
-  )
-}
-
-export default Hero
+          
+        </StyledHero>
+      </>
+    )
+  }
+  
+  export default Hero
+  
