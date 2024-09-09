b787d2c5646dd7ca5a6e524ccd5a1700aa90c1b8
dmourasi <dmouraslv@gmail.com>
2024-09-09 18:30:24 -0300

    correct grid

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -1,36 +1,30 @@
-import {styled } from "@mui/material"
-import Avatar from "../../../../assets/imagens/avatar.jpg"
-import * as material from "@mui/material";
-
+import { Grid2, styled } from "@mui/material";
+import Avatar from "../../../../assets/imagens/avatar.jpg";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(()=> ({
-        backgroundColor: "black"
-    }))
+    const StyledHero = styled("div")(() => ({
+        backgroundColor: "black",
+        padding: "20px"
+    }));
     
-    const StyledImg = styled("img")(()=> ({
-        with: "30%",
+    const StyledImg = styled("img")(() => ({
+        width: "30%",
         borderRadius: "50%"
-    }))
+    }));
 
     return (
-      <>
-        <StyledHero>
-          
-          <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <StyledImg src={Avatar} />
-            </Grid>
-            <Grid item xs={8}>
-              xs=4
-            </Grid>
-          </Grid>
-          
-        </StyledHero>
-      </>
-    )
-  }
-  
-  export default Hero
-  
+      <StyledHero>
+        <Grid2 container spacing={2}>
+          <Grid2 item xs={4}>
+            <StyledImg src={Avatar} alt="Avatar" />
+          </Grid2>
+          <Grid2 item xs={8}>
+            xs=8
+          </Grid2>
+        </Grid2>
+      </StyledHero>
+    );
+};
+
+export default Hero;
