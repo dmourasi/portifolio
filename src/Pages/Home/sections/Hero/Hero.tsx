bc5169b4d17214acc2a786de8db95890b7d21cf4
dmourasi <dmouraslv@gmail.com>
2024-09-09 19:49:46 -0300

    grid2 and buttons

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -1,28 +1,47 @@
-import { Grid2, styled } from "@mui/material";
+import { Button, Container, Grid2, styled, Typography} from "@mui/material";
 import Avatar from "../../../../assets/imagens/avatar.jpg";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
         backgroundColor: "black",
-        padding: "20px"
+        height: "100vh"
     }));
     
     const StyledImg = styled("img")(() => ({
-        width: "30%",
-        borderRadius: "50%"
+        width: "100%",
+        borderRadius: "70%"
     }));
 
     return (
       <StyledHero>
-        <Grid2 container spacing={2}>
-          <Grid2 item xs={4}>
-            <StyledImg src={Avatar} alt="Avatar" />
+        <Container maxWidth="lg">
+          <Grid2 container spacing={2}>
+            <Grid2 size={{xs:12, md:4}}>
+              <StyledImg src={Avatar} alt="Avatar" />
+            </Grid2>
+            <Grid2 size={{xs:12, md:8}}>
+              <Typography color="primary" variant="h1" textAlign="center">Daniel Moura</Typography>
+              <Typography color="primary" variant="h2" textAlign="center">I'am a Data Scientist</Typography>
+              <Grid2 container  display="flex" justifyContent="center">
+                <Grid2 size={{xs:16, md:4}} display="flex" justifyContent="center">
+                  <Button>
+                  <FileDownloadIcon />
+                  Download CV
+                  </Button>
+                </Grid2>
+                <Grid2 size={{xs:16, md:4}} display="flex" justifyContent="center">
+                  <Button>
+                  <MailOutlineIcon />
+                  Contate me
+                  </Button>
+                </Grid2>
+              </Grid2>            
+            </Grid2>
           </Grid2>
-          <Grid2 item xs={8}>
-            xs=8
-          </Grid2>
-        </Grid2>
+        </Container>
       </StyledHero>
     );
 };
