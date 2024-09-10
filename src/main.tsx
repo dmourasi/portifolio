13810e2f69fa9c46a678e7a3ae8b57a9eae91b50
dmourasi <dmouraslv@gmail.com>
2024-09-09 21:55:02 -0300

    theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Css } from '@mui/icons-material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import theme from './theme.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+       
+  </React.StrictMode>,
 )
