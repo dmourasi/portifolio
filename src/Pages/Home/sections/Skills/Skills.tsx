b60f663310bcb422c1c3a3081e709e86ff460071
Daniel Moura <dmouraslv@gmail.com>
2024-10-22 10:21:02 -0300

    adding icons skills

--- src/Pages/Home/sections/Skills/Skills.tsx
+++ src/Pages/Home/sections/Skills/Skills.tsx
@@ -1,10 +1,25 @@
 import { Box, Container, Grid2, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 
+// Importando os ícones diretamente
+import pythonIcon from './technical/python.webp';
+import rIcon from './technical/r_project.webp';
+import perlIcon from './technical/perl.png';
+import sqlIcon from './technical/sql.png';
+import scrumIcon from './technical/scrum.png';
+import machineIcon from './technical/machine.png';
+
+// Ícones de Expertise Topics
+import biostatsIcon from './expertise/biostats.png';
+import computIcon from './expertise/comput.png';
+import enviIcon from './expertise/envi.png';
+import forensicIcon from './expertise/forensic.png';
+import microIcon from './expertise/micro.png';
+import physioIcon from './expertise/physio.png';
+
 const Skills = () => {
-  // Estilo principal da seção de Skills
   const StyledSkills = styled("div")(({ theme }) => ({
-    backgroundColor: "#000", // Fundo preto
+    backgroundColor: "#000",
     minHeight: "100vh",
     display: "flex",
     alignItems: "center",
@@ -12,26 +27,32 @@ const Skills = () => {
     paddingBottom: "100px",
   }));
 
-  // Estilo dos quadrantes
   const SkillBox = styled(Box)(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main, // Cor principal do tema
+    backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    padding: "40px 20px", // Aumentando o padding para dar mais espaçamento
-    borderRadius: "12px", // Bordas arredondadas para dar suavidade
+    padding: "40px 20px",
+    borderRadius: "12px",
     display: "flex",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
     textAlign: "center",
     fontFamily: 'Nexa',
     fontSize: "18px",
     fontWeight: "bold",
-    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.4)", // Sombra para criar profundidade
+    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.4)",
     transition: "transform 0.4s ease, background-color 0.4s ease, box-shadow 0.4s ease",
-    cursor: "pointer", // Mudando o cursor ao passar o mouse
+    cursor: "pointer",
     "&:hover": {
-      transform: "scale(1.1)", // Efeito de zoom ao passar o mouse
-      backgroundColor: theme.palette.secondary.main, // Mudança de cor no hover
-      boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.7)", // Sombra mais intensa no hover
+      transform: "scale(1.1)",
+      backgroundColor: theme.palette.secondary.main,
+      boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.7)",
+    },
+    "& img": {
+      marginBottom: "12px",
+    },
+    "& span": {
+      fontSize: "20px", // Aumentando o tamanho da fonte
     },
   }));
 
@@ -46,7 +67,7 @@ const Skills = () => {
           Skills
         </Typography>
 
-        <Grid2 container spacing={4} display="flex" flexDirection="column" alignItems="center">
+        <Grid2 container spacing={4}>
           {/* Habilidades técnicas */}
           <Grid2 size={{ xs: 12, md: 6 }} display="flex" flexDirection="column" alignItems="center">
             <Typography variant="h4" color="primary.contrastText" gutterBottom sx={{ fontFamily: 'Nexa', marginBottom: '16px', textAlign: 'center', fontSize: '28px' }}>
@@ -54,50 +75,85 @@ const Skills = () => {
             </Typography>
             <Grid2 container spacing={3} justifyContent="center">
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>Python</SkillBox>
+                <SkillBox>
+                  <img src={pythonIcon} alt="Python" width="50" height="50" />
+                  <span>Python</span>
+                </SkillBox>
               </Grid2>
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>R</SkillBox>
+                <SkillBox>
+                  <img src={rIcon} alt="R" width="50" height="50" />
+                  <span>R</span>
+                </SkillBox>
               </Grid2>
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>Perl</SkillBox>
+                <SkillBox>
+                  <img src={perlIcon} alt="Perl" width="50" height="50" />
+                  <span>Perl</span>
+                </SkillBox>
               </Grid2>
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>SQL</SkillBox>
+                <SkillBox>
+                  <img src={sqlIcon} alt="SQL" width="50" height="50" />
+                  <span>SQL</span>
+                </SkillBox>
               </Grid2>
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>Linux</SkillBox>
+                <SkillBox>
+                  <img src={scrumIcon} alt="Scrum" width="50" height="50" />
+                  <span>Scrum</span>
+                </SkillBox>
               </Grid2>
               <Grid2 size={{ xs: 12, sm: 6 }}>
-                <SkillBox>Machine Learning</SkillBox>
+                <SkillBox>
+                  <img src={machineIcon} alt="Machine Learning" width="50" height="50" />
+                  <span>Machine Learning</span>
+                </SkillBox>
               </Grid2>
             </Grid2>
           </Grid2>
-        </Grid2>
-        <Grid2 container spacing={4} display="flex" flexDirection="column" alignItems="center">
+
           {/* Tópicos de conhecimento */}
           <Grid2 size={{ xs: 12, md: 6 }} display="flex" flexDirection="column" alignItems="center">
-            <Typography variant="h4" color="primary.contrastText" gutterBottom sx={{ fontFamily: 'Nexa', marginBottom: '16px', textAlign: 'center', fontSize: '20px' }}>
+            <Typography variant="h4" color="primary.contrastText" gutterBottom sx={{ fontFamily: 'Nexa', marginBottom: '16px', textAlign: 'center', fontSize: '28px' }}>
               Expertise Topics
             </Typography>
-            <Grid2 container spacing={2} justifyContent="center">
-            <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Biostatistics</SkillBox>
+            <Grid2 container spacing={3} justifyContent="center">
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={biostatsIcon} alt="Biostatistics" width="50" height="50" />
+                  <span>Biostatistics</span>
+                </SkillBox>
               </Grid2>
-              <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Computacional Biology</SkillBox>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={computIcon} alt="Computational Biology" width="50" height="50" />
+                  <span>Computational Biology</span>
+                </SkillBox>
               </Grid2>
-              <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Physiology and Behavior</SkillBox>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={physioIcon} alt="Physiology & Behavior" width="50" height="50" />
+                  <span>Physiology & Behavior</span>
+                </SkillBox>
               </Grid2>
-              <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Microbiology</SkillBox>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={enviIcon} alt="Environmental Science" width="50" height="50" />
+                  <span>Environmental Science</span>
+                </SkillBox>
               </Grid2>
-              <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Environmetal Science</SkillBox>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={microIcon} alt="Microbiology" width="50" height="50" />
+                  <span>Microbiology</span>
+                </SkillBox>
               </Grid2>
-              <Grid2 size={{ xs: 8, sm: 4 }}>
-                <SkillBox>Forensic Biology</SkillBox>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <SkillBox>
+                  <img src={forensicIcon} alt="Forensic Biology" width="50" height="50" />
+                  <span>Forensic Biology</span>
+                </SkillBox>
               </Grid2>
             </Grid2>
           </Grid2>
