59ccb29660644a44b0ce6540ac2925ab99f5ee49
Daniel Moura <dmouraslv@gmail.com>
2024-11-04 15:49:49 -0300

    drawer menu update

--- src/compoents/NaviBar/NaviBar.tsx
+++ src/compoents/NaviBar/NaviBar.tsx
@@ -7,26 +7,40 @@ const NaviBar = () => {
 
   const StyledToolbar = styled(Toolbar)(({ theme }) => ({
     display: "flex",
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
     minHeight: "40px",
     padding: "0 20px",
     marginBottom: "-10px",
     boxShadow: "none",
+    backgroundColor: "#30000", // Cor de fundo da Navbar (cinza escuro)
     [theme.breakpoints.down('sm')]: {
-      justifyContent: "space-between", // Alinha para ícone de menu
       padding: "10px 20px",
     },
-  })); 
+  }));
 
   const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
     fontSize: "16px",
-    padding: "6px 70px",
+    padding: "10px 0", // Ajusta o padding vertical para centralização
+    textAlign: "center",
+    flex: 1, // Faz cada item ocupar espaço igual
+    display: "flex", // Usar flexbox para centralização
+    justifyContent: "center", // Centraliza horizontalmente
+    alignItems: "center", // Centraliza verticalmente
+    color: "white", // Cor do texto
     '&:hover': {
-      backgroundColor: "#464646",
+      backgroundColor: "#464646", // Cor do fundo ao passar o mouse
     },
     [theme.breakpoints.down('sm')]: {
-      padding: "10px 20px", // Ajusta o padding em telas menores
+      padding: "10px 0", // Ajusta o padding em telas menores
       fontSize: "14px",
+    },
+  }));
+
+  // Estilizando o Drawer
+  const StyledDrawer = styled(Drawer)(({ theme }) => ({
+    '& .MuiDrawer-paper': {
+      backgroundColor: "#333333", // Cor de fundo do Drawer (cinza escuro)
+      color: "white", // Cor do texto do Drawer
     },
   }));
 
@@ -53,7 +67,7 @@ const NaviBar = () => {
           </IconButton>
 
           {/* Menu items para telas maiores */}
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" }, flexGrow: 1 }}>
             <StyledMenuItem onClick={() => handleScroll({ id: "about" })}>About</StyledMenuItem>
             <StyledMenuItem onClick={() => handleScroll({ id: "skills" })}>Skills</StyledMenuItem>
             <StyledMenuItem onClick={() => handleScroll({ id: "projects" })}>Projects</StyledMenuItem>
@@ -62,7 +76,7 @@ const NaviBar = () => {
       </AppBar>
 
       {/* Drawer para dispositivos móveis */}
-      <Drawer
+      <StyledDrawer
         anchor="left"
         open={drawerOpen}
         onClose={() => setDrawerOpen(false)}
@@ -78,7 +92,7 @@ const NaviBar = () => {
           <StyledMenuItem onClick={() => handleScroll({ id: "skills" })}>Skills</StyledMenuItem>
           <StyledMenuItem onClick={() => handleScroll({ id: "projects" })}>Projects</StyledMenuItem>
         </Box>
-      </Drawer>
+      </StyledDrawer>
     </>
   );
 };
